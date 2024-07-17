1d10fb0413f8292f8956e35b935c53308b7182db
Felipe Borges <felipetecacc@gmail.com>
2024-07-17 16:01:39 -0300

    Criando setup inicial e component header

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./Home.module.scss";
+import Header from "../components/header";
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.container}>
+        <Header />
+      </div> 
+    </>
+  );
+}
