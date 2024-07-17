b4a56e5a16b1c6271b95aa21b3fe3850be31fa7c
Felipe Borges <felipetecacc@gmail.com>
2024-07-17 17:04:07 -0300

    Criando sessão welcome

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import styles from "./Home.module.scss";
 import Header from "../components/header";
+import Welcome from "../components/welcome";
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
         <Header />
+        <Welcome />
       </div> 
     </>
   );
